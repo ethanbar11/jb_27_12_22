c7f20cedc6d8b05c57c17d30b7c6697e8d790f46
eyal <narnavg@gmail.com>
2022-12-27 20:11:59 +0200

    ff

---
+++ class_1/if_example.py
@@ -0,0 +1,15 @@
+x = 22
+y = 100
+if x == y:
+    print('X is equal to y')
+    print('Bla bla')
+elif x > y:
+    print('X is bigger than y')
+elif x > 20:
+    print('X is bigger than 20')
+elif x > 10:
+    print('X is bigger than 10')
+else:
+    print('X is not equal to y')
+print('This is going to be printed anyway')
+print('This is as well')
---
+++ class_1/if_string.py
@@ -0,0 +1,7 @@
+password = '12345678'
+
+user_password = input('Please enter your password: ')
+if user_password == password:
+    print('Correct!')
+else:
+    print('Bad password.')
