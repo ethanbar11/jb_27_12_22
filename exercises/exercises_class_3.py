39d3ef7825c3b1b0a79ba927479f3e2b3de06535
eyal <narnavg@gmail.com>
2023-01-06 11:46:04 +0200

    ff

--- exercises/exercises_class_3.py
+++
@@ -1,5 +0,0 @@
-https://www.practicepython.org/exercise/2014/03/26/08-rock-paper-scissors.html
-https://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
-
-Extra:
-https://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
