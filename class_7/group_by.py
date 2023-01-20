7861e753f5b745cd60d8504a1874851606b477d3
eyal <narnavg@gmail.com>
2023-01-20 12:28:46 +0200

    ff

---
+++ class_7/group_by.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+path = 'winemag-data-130k-v2.csv'
+df = pd.read_csv(path, index_col=0)
+
+
+print(df.groupby('country').points.mean().sort_values())
